e8cdbf55fbed8d971713fe5a6e1f0041c0aa7e43
Gustaf Oldertz <gustaf.oldertz@gmail.com>
2017-12-21 14:36:01 +0100

    Git-Test

---
+++ TDD.Grupparbete.MuseumTjuv1.0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Grupparbete.MuseumTjuv1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string green = "Funkar!";
+            Console.WriteLine("Git-Test: ", green);
+
+        }
+    }
+}
