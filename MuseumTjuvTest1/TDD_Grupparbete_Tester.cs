667676da51696e43cc8cd3448a441506ecfc60d5
Gustaf Oldertz <gustaf.oldertz@gmail.com>
2017-12-21 19:22:55 +0100

    Uppdaterat test

---
+++ MuseumTjuvTest1/TDD_Grupparbete_Tester.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDD.Grupparbete.MuseumTjuv1._0;
+
+namespace MuseumTjuvTest1
+{
+    [TestFixture]
+    public class TDD_Grupparbete_Tester
+    {
+        [Test]
+        public void GameTest()
+        {
+            //Arrange
+            int posXY = 1;
+
+            //Act
+
+            Game game = new Game();
+            int output = game.gameboard[0, 0];
+
+            //Assert
+            Assert.AreEqual(posXY, output);
+            
+        }
+    }
+}
